a892263374001433be523164569a5713ae910f9d
Carissella <carissebarrswe@gmail.com>
2023-03-09 05:32:19 -0500

    added server logic to serverJS file, added app.use and app.listen property

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const path = require("path");
-const htmlRoutes = require("");
-const apiRoutes = require("");
+const htmlRoute = require("");
+const apiRoute = require("");
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("Develop/public"));
+app.use("/", htmlRoute);
+app.use("/api", apiRoute);
+
+app.listen(PORT, () =>
+    console.log(`This app is listening at http://localhost:${PORT} yay!`));
